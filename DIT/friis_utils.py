a39d3a125dfa29db2288626e7009a6119442c713
Hartvi <j.hartvich@gmail.com>
2023-01-04 23:06:45 +0100

    finish SSUhw03, start DIT dipole antenna

--- DIT/friis_utils.py
+++ DIT/friis_utils.py
@@ -1,18 +1,30 @@
-
 from scipy.spatial.transform import Rotation
-from typing import *
 import numpy as np
 import math as mt
 
+from scipy import integrate as itg
+from scipy import optimize as opt
+from typing import *
+import itertools as it
+
 from class_w_units import Orientation
+from friis_utils import *
 
 npr = np.array
+vized = lambda x: np.vectorize(x, [float])
+
+PI = np.pi
+TOOPI = 2*PI
+HALFPI = PI/2
+SQRT2 = mt.sqrt(2)
+ISOTROPIC_AMPLITUDE_2D = 1/TOOPI          # for convenience the integral over the circle is 1
+ISOTROPIC_AMPLITUDE_3D = 1/2/mt.sqrt(PI)  # for convenience the integral over the surface is 1
+
 c = 299792458.0
-TOOPI = np.pi*2
 
 
 class Antenna:
-    def __init__(self, frequency, power_funcs, antenna_rotation, base_gains) -> None:
+    def __init__(self, frequency, power_funcs, antenna_rotation, base_gain) -> None:
         self.frequency = frequency
         self.wave_length = c/frequency
         # should be a list of the power funcs for each axis xyz
@@ -20,39 +32,82 @@ class Antenna:
         self.antenna_rotation = antenna_rotation
         # vector points up to correspond to default antenna rotation
         self.polarization_vector = antenna_rotation @ npr([[0, 0, 1]]).T
-        self.base_gains = base_gains
+        self.direction_vector = antenna_rotation @ npr([[0, 1, 0]]).T
+        self.base_gain = base_gain
     
     def eval(self, xyz, logify=False, include_amplitude=True):
-        x,y,z = xyz
-        # ro = np.sqrt(x**2+y**2)
+        x,y,z = self.antenna_rotation @ xyz
         
         angles = npr([np.arctan2(z, y), np.arctan2(x, z), np.arctan2(y, x)])
-        # xyz_sqr = xyz * xyz
-        # sqr_sum = np.sum(xyz_sqr, axis=0)
-        # dists = np.sqrt(sqr_sum)
-        # factors = xyz_sqr / sqr_sum
         dists = np.linalg.norm(xyz, axis=0)
         
-        # print("angles:", angles)
-        # print('factors:',factors)
-        power_gains = self.power_funcs(angles)
-        # print(self.base_gains)
-        # print((power_gains.T))
-        # print((power_gains.T*self.base_gains).T)
-        # OR DOES THIS HAVE TO BE WEIGHTED BY THE SQUARE??
-        power_gain = np.prod((power_gains.T*self.base_gains).T, axis=0)
-        # print('power gain:', power_gain)
-        # dist = np.sqrt(ro**2+z)
+        power_gains = self.power_funcs(angles)  # (3, n)
+        power_gain = np.prod(power_gains, axis=0)*self.base_gain  # (n, )
+        # print(power_gain.shape)
         phases = (dists % self.wave_length)*TOOPI
-        # print('polarization', self.polarization_vector)
-        # print('phases', np.sin(phases))
-        value_vector = (np.sin(phases) * self.polarization_vector)
-        # print('polarization vectors', value_vector)
+        amplitude_vector = (np.sin(phases) * self.polarization_vector)
         amplitudes = (self.wave_length/(4*np.pi*dists))**2
         if logify:
-            return 10*np.log10(power_gain) + 10*np.log10((amplitudes if include_amplitude else 1)), value_vector
+            return 10*np.log10(power_gain) + 10*np.log10((amplitudes if include_amplitude else 1)), amplitude_vector
         else:
-            return power_gain*(amplitudes if include_amplitude else 1), value_vector
+            return power_gain*(amplitudes if include_amplitude else np.ones(amplitudes.shape)), amplitude_vector
+
+
+class PatchAntenna(Antenna):
+    def __init__(self, antenna_rotation, frequency=915e6, base_gain=4.8, hpbw_z=120.0, hpbw_x=62.0, balance_z=0.95, balance_x=0.95) -> None:
+
+        max_power = 4.8
+        # NOTE: y is the direction it is radiating in, thus it doesn't affect others, i.e. unity gain
+        # base_gains = npr([np.sqrt(max_power), 1, np.sqrt(max_power)])
+        hpbw = hpbw_z/180.0*PI
+        antenna_func_z = find_figO(hpbw, balance=balance_z)
+
+        hpbw = hpbw_x/180.0*PI
+        antenna_func_x = find_figO(hpbw, balance=balance_x)
+        antenna_func_x = cosinify(antenna_func_x)
+
+        antenna_func_y = vized(lambda _: 1.0)
+        power_funcs = npr([antenna_func_x, antenna_func_y, antenna_func_z])
+        
+        super().__init__(frequency, power_funcs, antenna_rotation, base_gain)
+
+
+class RodAntenna(Antenna):  # TODO: finish the power funcs so that it takes ony the angle between z and the xy plane and has a radially symmetrical radiation characteristic
+    def __init__(self, antenna_rotation, frequency=915e6, base_gain=1.5, hpbw_y=80.0, hpbw_x=80.0, balance_y=0.5, balance_x=0.5) -> None:
+
+        max_power = 4.8
+        # NOTE: y is the direction it is radiating in, thus it doesn't affect others, i.e. unity gain
+        # base_gains = npr([np.sqrt(max_power), 1, np.sqrt(max_power)])
+        hpbw = hpbw_y/180.0*PI
+        antenna_func_y = find_figO(hpbw, balance=balance_y)
+        antenna_func_y = cosinify(antenna_func_y)
+
+        hpbw = hpbw_x/180.0*PI
+        antenna_func_x = find_figO(hpbw, balance=balance_x)
+        antenna_func_x = cosinify(antenna_func_x)
+
+        antenna_func_z = vized(lambda _: 1.0)
+        power_funcs = npr([antenna_func_x, antenna_func_y, antenna_func_z])
+        
+        super().__init__(frequency, power_funcs, antenna_rotation, base_gain)
+
+    def eval(self, xyz, logify=False, include_amplitude=True):
+        x,y,z = self.antenna_rotation @ xyz
+        
+        angle = np.arctan2([z, np.sqrt(x**2+y**2)])
+        dists = np.linalg.norm(xyz, axis=0)
+        
+        power_gains = self.power_funcs(angles)  # (3, n)
+        power_gain = np.prod(power_gains, axis=0)*self.base_gain  # (n, )
+        # print(power_gain.shape)
+        phases = (dists % self.wave_length)*TOOPI
+        amplitude_vector = (np.sin(phases) * self.polarization_vector)
+        amplitudes = (self.wave_length/(4*np.pi*dists))**2
+        if logify:
+            return 10*np.log10(power_gain) + 10*np.log10((amplitudes if include_amplitude else 1)), amplitude_vector
+        else:
+            return power_gain*(amplitudes if include_amplitude else np.ones(amplitudes.shape)), amplitude_vector
+
 
 
 class WaveUnit(Orientation):
@@ -63,5 +118,144 @@ class WaveUnit(Orientation):
     
     def step():
         ...
+
+
+def polar2euclid(theta, r):
+    sins = np.sin(theta)
+    coss = np.cos(theta)
+    return r*coss, r*sins
+
+
+def euclid2polar(x, y):
+    return np.linalg.norm([x-y], axis=0), np.atan2([y, x], axis=0)
+
+
+def normalized_to_2pi(func):
+    my_integral, err = itg.quad(func, 0, 2*PI)
+    return lambda x: func(x) / my_integral
+
+
+def figure8(balance, squeeze=0, squeeze_limit=0.9, is_sine=True):
+
+    # print('balance', balance, 'squeeze', squeeze)
+    # assert 0<=balance<=1, "balance must be in interval [0,1]"
+    balance = np.clip(balance, 0, 1)
+    counter_balance = balance - 1
+    squeeze_limit = np.clip(squeeze_limit, 0, 1)
+    squeeze = np.clip(squeeze, 0, squeeze_limit)
+    squeeze *= PI
+    half_squeeze = 0.5*squeeze
+    limits = [half_squeeze, PI-half_squeeze, PI+half_squeeze, TOOPI-half_squeeze]
+    speed_up_factor = PI/(PI-squeeze)
+
+    def get_angle(x):
+        # x = x
+        if x < limits[0]:
+            angle = 0
+        elif x < limits[1]:
+            angle = speed_up_factor*(x-half_squeeze)
+        elif x < limits[2]:
+            angle = PI
+        elif x < limits[3]:
+            angle = speed_up_factor*(x-squeeze-half_squeeze)
+        else:
+            angle = TOOPI
+        return angle
+
+    def func(x):
+        angle = get_angle(x)
+        if is_sine:
+            if x < PI:
+                return balance*mt.sin(angle)
+            else:
+                return counter_balance*mt.sin(angle)
+        else:
+            if x < HALFPI or x > HALFPI+PI:
+                return balance*mt.cos(angle)
+            else:
+                return counter_balance*mt.cos(angle)
+    return func
+
+
+def const_func(x):
+    if isinstance(x, np.ndarray):
+        return npr([0.5/PI]*len(x))
+    else:
+        return 0.5/PI
+
+
+def create_func(func):
+    return normalized_to_2pi(vized(func))
+
+
+def normalized_plot(ax, x, y):
+    ax.plot(x/np.max(x), y)
+
+
+def cumul_int(y, x):
+    return itg.cumulative_trapezoid(y, x, initial=0)
     
+
+def find_func(hpbw, max_amplitude, is_sine=True):
+
+    hhpbw = hpbw/2
+    print("PI-hpbw/2:", (HALFPI - hhpbw)/PI*180)
+    half_max = max_amplitude/SQRT2
+    def err_func(arg):
+        err = 0.0
+        new_func = lambda x: (create_func(figure8(arg[0], arg[1], is_sine=is_sine))(x))
+        # err = new_func(HALFPI)
+        # err = (err - max_amplitude)**2
+        err += (new_func(HALFPI - hhpbw) - half_max)**2
+        # err += 0.001*(new_func(3*HALFPI))**2
+        return err
+    return err_func
+
+
+def figureO(squeeze, squeeze_limit=0.9, balance=0.95, is_sine=True):
+    return figure8(balance, squeeze, squeeze_limit, is_sine=is_sine)
+
+
+def find_figO(hpbw, balance=0.95, is_sine=True):
+    hhpbw = hpbw/2
+    sqrt_balance = 1 - (1-balance)**2
+    # print("PI-hpbw/2:", (HALFPI - hhpbw)/PI*180)
+    def err_func(arg):
+        new_func = lambda x: figureO(arg, balance=balance)(x)
+        err = (new_func(HALFPI) - new_func(HALFPI - hhpbw)*2)**2  # HALF AMPLITUDE => SQRT TO GET POWER RATIO
+        return err
+    fv = vized(err_func)
+    resolution = 1001
+    l = np.linspace(0, 1, resolution)
+    fvl = fv(l)
+    opt_i = np.argmin(fvl)
+    opt_res = opt_i/resolution
+    # print('min err:', fvl[opt_i])
+    # print("found result:", opt_res)
+    
+    return vized(lambda x: figureO(opt_res, balance=balance, is_sine=is_sine)(x)/balance)
+
+
+def maxify_func(func, amplitude):
+    k = amplitude/func(HALFPI)
+    return lambda x: k*func(x)
+
+
+def cosinify(func):
+    return lambda x: func(x+HALFPI)
+
+
+def create_func_w_hpbw(hpbw, deg=True, func_type='o', func_params={'balance': 0.95}, cosine=False):
+    
+    if deg:
+        hpbw /= 180.0*PI
+    if func_type == 'o':
+        antenna_func = find_figO(hpbw, **func_params)
+    else:
+        raise NotImplementedError('Figure 8 func hasn\'t been implemented yet')
+        antenna_func = find_fig8(hpbw, **func_params)
+
+    if cosine:
+        antenna_func = cosinify(antenna_func)
+    ...
 
--- DIT/plotting.py
+++ DIT/plotting.py
@@ -12,91 +12,6 @@ import itertools as it
 from class_w_units import Orientation
 from friis_utils import *
 
-npr = np.array
-vized = lambda x: np.vectorize(x, [float])
-
-PI = np.pi
-TOOPI = 2*PI
-HALFPI = PI/2
-SQRT2 = mt.sqrt(2)
-ISOTROPIC_AMPLITUDE_2D = 1/TOOPI          # for convenience the integral over the circle is 1
-ISOTROPIC_AMPLITUDE_3D = 1/2/mt.sqrt(PI)  # for convenience the integral over the surface is 1
-
-
-def polar2euclid(theta, r):
-    sins = np.sin(theta)
-    coss = np.cos(theta)
-    return r*coss, r*sins
-
-
-def euclid2polar(x, y):
-    return np.linalg.norm([x-y], axis=0), np.atan2([y, x], axis=0)
-
-
-def normalized_to_2pi(func):
-    my_integral, err = itg.quad(func, 0, 2*PI)
-    return lambda x: func(x) / my_integral
-
-
-def figure8(balance, squeeze=0, squeeze_limit=0.9, is_sine=True):
-
-    # print('balance', balance, 'squeeze', squeeze)
-    # assert 0<=balance<=1, "balance must be in interval [0,1]"
-    balance = np.clip(balance, 0, 1)
-    counter_balance = balance - 1
-    squeeze_limit = np.clip(squeeze_limit, 0, 1)
-    squeeze = np.clip(squeeze, 0, squeeze_limit)
-    squeeze *= PI
-    half_squeeze = 0.5*squeeze
-    limits = [half_squeeze, PI-half_squeeze, PI+half_squeeze, TOOPI-half_squeeze]
-    speed_up_factor = PI/(PI-squeeze)
-
-    def get_angle(x):
-        # x = x
-        if x < limits[0]:
-            angle = 0
-        elif x < limits[1]:
-            angle = speed_up_factor*(x-half_squeeze)
-        elif x < limits[2]:
-            angle = PI
-        elif x < limits[3]:
-            angle = speed_up_factor*(x-squeeze-half_squeeze)
-        else:
-            angle = TOOPI
-        return angle
-
-    def func(x):
-        angle = get_angle(x)
-        if is_sine:
-            if x < PI:
-                return balance*mt.sin(angle)
-            else:
-                return counter_balance*mt.sin(angle)
-        else:
-            if x < HALFPI or x > HALFPI+PI:
-                return balance*mt.cos(angle)
-            else:
-                return counter_balance*mt.cos(angle)
-    return func
-
-
-def const_func(x):
-    if isinstance(x, np.ndarray):
-        return npr([0.5/PI]*len(x))
-    else:
-        return 0.5/PI
-
-
-def create_func(func):
-    return normalized_to_2pi(vized(func))
-
-
-def normalized_plot(ax, x, y):
-    ax.plot(x/np.max(x), y)
-
-
-def cumul_int(y, x):
-    return itg.cumulative_trapezoid(y, x, initial=0)
 
 
 def plot_func(func, samples=100):
@@ -121,80 +36,15 @@ def plot_func(func, samples=100):
     # ax = fig.add_subplot(projection='3d')
 
 
-def find_func(hpbw, max_amplitude, is_sine=True):
-
-    hhpbw = hpbw/2
-    print("PI-hpbw/2:", (HALFPI - hhpbw)/PI*180)
-    half_max = max_amplitude/SQRT2
-    def err_func(arg):
-        err = 0.0
-        new_func = lambda x: (create_func(figure8(arg[0], arg[1], is_sine=is_sine))(x))
-        # err = new_func(HALFPI)
-        # err = (err - max_amplitude)**2
-        err += (new_func(HALFPI - hhpbw) - half_max)**2
-        # err += 0.001*(new_func(3*HALFPI))**2
-        return err
-    return err_func
-
-
-def figureO(squeeze, squeeze_limit=0.9, balance=0.95, is_sine=True):
-    return figure8(balance, squeeze, squeeze_limit, is_sine=is_sine)
-
-
-def find_figO(hpbw, balance=0.95, is_sine=True):
-    hhpbw = hpbw/2
-    sqrt_balance = 1 - (1-balance)**2
-    # print("PI-hpbw/2:", (HALFPI - hhpbw)/PI*180)
-    def err_func(arg):
-        new_func = lambda x: figureO(arg, balance=balance)(x)
-        err = (new_func(HALFPI) - new_func(HALFPI - hhpbw)*2)**2  # HALF AMPLITUDE => SQRT TO GET POWER RATIO
-        return err
-    fv = vized(err_func)
-    resolution = 1001
-    l = np.linspace(0, 1, resolution)
-    fvl = fv(l)
-    opt_i = np.argmin(fvl)
-    opt_res = opt_i/resolution
-    # print('min err:', fvl[opt_i])
-    # print("found result:", opt_res)
-    
-    return vized(lambda x: figureO(opt_res, balance=balance, is_sine=is_sine)(x)/balance)
-
-
-def maxify_func(func, amplitude):
-    k = amplitude/func(HALFPI)
-    return lambda x: k*func(x)
-
-
-def cosinify(func):
-    return lambda x: func(x+HALFPI)
-
-
-if __name__ == '__main__':    
-
-    max_amplitude = 4.8
-    hpbw = 120.0/180.0*PI
-    antenna_func_z = find_figO(hpbw, balance=0.95)
-    # make func have a preset forward max gain
-    # antenna_func_z = maxify_func(antenna_func_z, max_amplitude)
-    # print("max val:", antenna_func_z(HALFPI), ' half val:', antenna_func_z(HALFPI-hpbw/2))
-    # plot_func(antenna_func_z)
-
-    hpbw = 60.0/180.0*PI
-    antenna_func_x = find_figO(hpbw, balance=0.95)
-    # antenna_func_x = maxify_func(antenna_func_x, max_amplitude)
-    # print("max val:", antenna_func_x(HALFPI), ' half val:', antenna_func_x(HALFPI-hpbw/2))
-    antenna_func_x = cosinify(antenna_func_x)
-    # plot_func(antenna_func_x)
-    # exit(1)
-
-    antenna_func_y = vized(lambda _: 1.0)
-    funcs = npr([antenna_func_x, antenna_func_y, antenna_func_z])
-    antenna = Antenna(915e6, funcs, np.eye(3), base_gains=npr([np.sqrt(4.8), 1, np.sqrt(4.8)]))
+def plot_antenna_characteristic_3d(antenna: Antenna):
+    # antenna = Antenna(915e6, funcs, np.eye(3), base_gains=npr([np.sqrt(4.8), 1, np.sqrt(4.8)]))
     number_of_sections = 40
     cube_side = 1
+    # x_range = (-2*cube_side, 2*cube_side)
+    # y_range = (0, 4*cube_side)
+    # z_range = (-2*cube_side, 2*cube_side)
     x_range = (-2*cube_side, 2*cube_side)
-    y_range = (0, 4*cube_side)
+    y_range = (-2*cube_side, 2*cube_side)
     z_range = (-2*cube_side, 2*cube_side)
     eval_space_range_x = np.linspace(*x_range, number_of_sections)
     eval_space_range_y = np.linspace(*y_range, number_of_sections//2)
@@ -202,7 +52,7 @@ if __name__ == '__main__':
     eval_positions = npr(list(it.product(eval_space_range_x, eval_space_range_y, eval_space_range_z))).T
     cube_range = np.max(eval_positions)
     cube_range = (-cube_range, cube_range)
-    print('eval positions', eval_positions.shape[1])
+    # print('eval positions', eval_positions.shape[1])
     # eval_positions = npr([eval_space_range]*3)
 
     # print(eval_positions.shape)
@@ -267,6 +117,31 @@ if __name__ == '__main__':
         freq_slider.on_changed(update)
 
         plt.show()
+
+
+if __name__ == '__main__':    
+
+    # max_power = 4.8
+    # hpbw = 120.0/180.0*PI
+    # antenna_func_z = find_figO(hpbw, balance=0.95)
+
+    # hpbw = 62.0/180.0*PI
+    # antenna_func_x = find_figO(hpbw, balance=0.95)
+    # antenna_func_x = cosinify(antenna_func_x)
+
+    # antenna_func_y = vized(lambda _: 1.0)
+    # funcs = npr([antenna_func_x, antenna_func_y, antenna_func_z])
+    # antenna = Antenna(915e6, funcs, np.eye(3), base_gains=npr([np.sqrt(max_power), 1, np.sqrt(max_power)]))
+    
+    # hpbw = 80.0/180.0*PI
+    # antenna_func_z = find_figO(hpbw, balance=0.5)
+    # plot_func(antenna_func_z)
+    # exit(1)
+    norot = np.eye(3)
+    zrot = npr([[0, 1, 0], [-1, 0, 0], [0, 0, 1]])
+    # antenna = PatchAntenna(zrot)
+    antenna = RodAntenna(norot)
+    plot_antenna_characteristic_3d(antenna=antenna)
     
     # lnsp = np.linspace(0, TOOPI, 100)
     # sine = np.sin(lnsp)
